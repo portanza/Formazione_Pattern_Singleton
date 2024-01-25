eb530b17b6901d2876a53634358ed06da0203d60
Luca <42120435+portanza@users.noreply.github.com>
2024-01-25 12:41:06 +0100

    Aggiungere i file di progetto.

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿
+using System;
+
+// Il pattern Singleton è utilizzato per eliminare la possibilità che una classe venga instanziata più volte.
+// In pratica la rendiamo statica per non doverla creare dall'esterno, viene creata una sola volta se non esiste
+// già. Pensa alla classe che gestisce i codici univoci di una banca: non possiamo permetterci il rischio di avere due 
+// codici uguali.
+
+namespace SingletonEasy
+{
+	class MyClass
+	{
+
+		static private MyClass? instance = null;
+		static public MyClass Instance
+		{
+			get { 
+				if (instance == null) instance= new MyClass();	
+				return instance; }	
+			// return instanc ??= new MyClass();  sostituisce le due righe precedenti.
+		}
+		private MyClass() { Console.WriteLine("Oggetto creato"); }
+
+
+		public void Metodo()
+		{
+			Console.WriteLine("Metodo richiamato");
+		}
+
+		
+
+	}
+
+	class Program
+	{
+		static void Main(string[] args)
+		{
+            //Mostra "Oggetto creato" solo con la prima chiamata a Metodo(), che 
+            MyClass.Instance.Metodo();
+            MyClass.Instance.Metodo();
+        }
+
+	}
+
+}
